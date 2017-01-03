d7dafc1a7f2d2b7cdedca068136e229fc8f46858
Mike Swartz <mikelswartz@gmail.com>
2017-01-03 11:06:16 -0500

    Added gitlab install code

--- gitlab.provision.sh
+++ gitlab.provision.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-yum update
-yum install -y vim git tree wget
 cd /vagrant/
-if [ -f "/vagrant/" ] 
- then
-  echo " exists"
- else
-  wget http://
-fi
-
+yum update
+yum install -y vim git tree 
+yum install curl openssh-server openssh-clients postfix cronie -y
+service postfix start
+chkconfig postfix on
+lokkit -s http -s ssh
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | /bin/bash
+yum install gitlab-ce -y
+#gitlab-ctl reconfigure
