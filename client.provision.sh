96c02520dbf97647200c2759b9a044cb35e2b1c5
Mike <mikelswartz@gmail.com>
2017-01-18 12:33:52 -0500

    Added 'ci' entry to client 'hosts' file.

--- client.provision.sh
+++ client.provision.sh
@@ -4,5 +4,6 @@ yum update
 yum install -y vim git tree wget
 
 echo "192.168.50.10 gitlab.home" >> /etc/hosts
+echo "192.168.50.10 ci.gitlab.home" >> /etc/hosts
 
 sudo -u vagrant ssh-keygen -t rsa -f "/home/vagrant/.ssh/id_rsa" -N '' -C "GitLab" -b 4096
